3432b6333f2c8c1659ea8e247801dd76d58d17cd
hro19 <fourbasic19@gmail.com>
2023-12-26 12:49:23 +0900

    送信フォームでメッセージを新規追加

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,31 +1,52 @@
 <template>
   <div>
-    <h1>チャットルーム {{ roomId }} </h1>
+    <h1>チャットルーム {{ roomId }}</h1>
 
     <ul>
       <li v-for="message in messages" :key="message.id">
         <strong>{{ message.sender_name }}:</strong> {{ message.content }}
       </li>
     </ul>
+    <hr />
+    <form @submit.prevent="sendMessage">
+      <div>
+        <h3>名前</h3>
+        <input type="text" v-model="senderName" placeholder="名前を入力" required />
+      </div>
+      <div>
+        <h3>メッセージ</h3>
+        <input
+          type="text"
+          v-model="newMessageContent"
+          placeholder="メッセージを入力"
+          required
+        />
+      </div>
+      <div>
+        <button type="submit">送信</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import axios from 'axios';
-import { defineComponent } from 'vue';
+import axios from "axios";
+import { defineComponent } from "vue";
 
 type Message = {
   id: number;
   sender_name: string;
   content: string;
-}
+};
 
 export default defineComponent({
-  props: ['roomId'],
+  props: ["roomId"],
   data() {
     return {
-      roomName: '',
+      roomName: "" as string,
       messages: [] as Message[],
+      senderName: "" as string,
+      newMessageContent: "" as string,
     };
   },
   created() {
@@ -35,13 +56,26 @@ export default defineComponent({
     fetchMessages() {
       axios
         .get(`http://localhost:3000/rooms/${this.roomId}/messages`)
-        .then(response => {
+        .then((response) => {
           this.messages = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
-    },
+      },
+    sendMessage() {
+      axios
+        .post(`http://localhost:3000/rooms/${this.roomId}/messages`, {
+          content: this.newMessageContent,
+          sender_name: this.senderName
+        })
+        .then(() => {
+          this.newMessageContent = ''
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
   },
 });
 </script>
